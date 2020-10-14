35cfc4c4adcb3f2b312b1fe334bd43e2e843fd7d
n2ptune <n2ptune@outlook.com>
2020-10-14 18:32:53 +0900

    fix: style popup page
    * fix: replaced manifest.json

--- src/packages/popup/popup.js
+++ src/packages/popup/popup.js
@@ -1,4 +1,15 @@
 chrome.storage.sync.get(['bookmark'], ({ bookmark }) => {
+  if (!bookmark.items.length) {
+    const parent = document.querySelector('.bookmark-list .list-wrapper')
+    const p = document.createElement('p')
+
+    p.style.color = 'rgba(0, 0, 0, 0.7)'
+    p.textContent =
+      '아직 즐겨찾기에 아무 것도 추가되지 않았습니다. 캐릭터 페이지 우측 하단의 버튼을 이용해 캐릭터를 즐겨찾기에 추가해보세요.'
+
+    insertItem(parent, p)
+  }
+
   bookmark.items.forEach((item) => {
     // Bookmark Template
     const parent = document.querySelector('.bookmark-list .list-wrapper')
@@ -9,6 +20,16 @@ chrome.storage.sync.get(['bookmark'], ({ bookmark }) => {
 })
 
 chrome.storage.sync.get(['recent'], ({ recent }) => {
+  if (!recent.items.length) {
+    const parent = document.querySelector('.recent-list .list-wrapper')
+    const p = document.createElement('p')
+
+    p.style.color = 'rgba(0, 0, 0, 0.7)'
+    p.textContent = '확장 프로그램을 설치한 이후의 최근 기록이 보여집니다.'
+
+    insertItem(parent, p)
+  }
+
   recent.items.forEach((item) => {
     // Recent Template
     const parent = document.querySelector('.recent-list .list-wrapper')
@@ -26,10 +47,7 @@ function insertItem(parent, item) {
   parent.appendChild(item)
 }
 
-function createNode(
-  { id, server, job, name, adventure, level },
-  nodeType = 'li'
-) {
+function createNode({ id, server, job, name, adventure }, nodeType = 'li') {
   const node = document.createElement(nodeType)
 
   node.classList.add('list-item')
@@ -44,9 +62,6 @@ function createNode(
     </span>
     <span class="adventure">
       ${adventure}
-    </span>
-    <span class="level">
-      레벨 ${level}
     </span>
   </a>
   `
